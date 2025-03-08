b6c541f443d24a2f89082e6ed3ce1e33ae493b4c
minnhiuu <koutachan1147@gmail.com>
2025-03-08 15:05:20 +0700

    merge login

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import SearchButton from '../SearchButton/SearchButton';
 import './Header.css'
+import LoginForm from "../../pages/Login/LogIn";
+import { useState } from 'react';
 
 function Header() {
+  const [showLogin, setShowLogin] = useState(false);
     return (
+      <>
       <header className="header d-flex justify-content-between align-items-center px-4 py-2">
         <div className="header-left d-flex align-items-center gap-3">
           <SearchButton/>
@@ -18,11 +22,19 @@ function Header() {
             </a>
         </div>
         <div className="header-right d-flex align-items-center gap-3">
-          <a href="#" className="text-dark fw-bold">Đăng nhập</a>
+          <a href="#" className="text-dark fw-bold" onClick={() => {setShowLogin(true)}}>Đăng nhập</a>
           <a href="#" className="text-dark fw-bold">Liên hệ</a>
           <a href="#" className="text-dark fw-bold">Giỏ hàng (1)</a>
         </div>
       </header>
+      {showLogin && <div className="overlay" onClick={() => setShowLogin(false)}></div>}
+
+      {showLogin && (
+          <div className="modal-container">
+              <LoginForm setShowLogin={setShowLogin} />
+          </div>
+      )}
+      </>
     );
   }
   export default Header;
